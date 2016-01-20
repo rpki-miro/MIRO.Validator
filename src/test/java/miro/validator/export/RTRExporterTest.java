f68719ddb1f85b86d7070aef5343ad9b205a4872
Andreas Reuter <andreas.router@gmail.com>
2016-01-20 20:18:09 +0100

    Add mockito and barebone RTRExporterTest

---
+++ src/test/java/miro/validator/export/RTRExporterTest.java
@@ -0,0 +1,14 @@
+package test.java.miro.validator.export;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RTRExporterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
